2270ec10a1f5481943d4902a0ad65635809d778e
leandroFFF <leandrofrancagti@gmail.com>
2022-11-09 03:14:53 -0300

    atualizacao

---
+++ modulo5_/src/collections/implementandoCollectionsEStreamsComJava/Comparacoes.java
@@ -0,0 +1,65 @@
+package collections.implementandoCollectionsEStreamsComJava;
+
+import java.awt.image.ImageProducer;
+import java.util.*;
+
+public class Comparacoes {
+    public static void main(String[] args) {
+        List<String> lista = new ArrayList<>();
+        lista.add("Leandro");
+        lista.add("Leonardo");
+        lista.add("Ladyjane");
+        lista.add("Eduardo");
+        lista.add("Lucas");
+
+        System.out.println(lista);
+        Collections.sort(lista);
+        System.out.println(lista);
+
+        List<Protuto> protutos = new ArrayList<>(){{
+            add(new Protuto("Maça", 5.60));
+            add(new Protuto("Goiaba", 4.23));
+            add(new Protuto("Acerola", 7.50));
+            add(new Protuto("Banana", 3.80));
+            add(new Protuto("Caju", 4.70));
+            add(new Protuto("Abacaxi", 3.00));
+        }};
+        Collections.sort(protutos);
+        System.out.println(protutos);
+
+
+    }
+    public static class Protuto implements Comparable<Protuto>{
+        private String nome;
+        private Double preco;
+
+        public String getNome() {
+            return this.nome;
+        }
+
+        public Double getPreco() {
+            return this.preco;
+        }
+
+        Protuto(String nome, Double preco) {
+            this.nome = nome;
+            this.preco = preco;
+        }
+
+        @Override
+        public String toString() {
+            return "{ " +
+                    "nome = '" + nome + '\'' +
+                    ", preco = " + preco +
+                    " }";
+        }
+
+        @Override
+        public int compareTo(Protuto o) {
+            if (this.getPreco() < o.getPreco()) return -1;
+            if (this.getPreco() > o.getPreco()) return 1;
+            return 0;
+        }
+    }
+}
+
--- modulo5_/src/collections/implementandoCollectionsEStreamsComJava/list/Comparators_.java
+++ modulo5_/src/collections/implementandoCollectionsEStreamsComJava/list/Comparators_.java
@@ -1,4 +1,58 @@
 package collections.implementandoCollectionsEStreamsComJava.list;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+
+/*
+* Comparators
+* interfaces que aprendemos
+*       java.util.Comparator - interface para definir classe com regra de ordenação
+*       java.util.Comparable - interface para sefinir regra de ordenacao em uma classe de dominio
+* Algoritmo de ordenacao
+* Utilizado primariamente em java.util.List
+* Permite a ordenacao de objetos complexos (criados pelo usuario)
+* */
 public class Comparators_ {
+    public static void main(String[] args) {
+        HashMap<String, Integer> alunos = new HashMap<>();
+        alunos.put("Leandro", 34);
+        alunos.put("Isabelle", 26);
+        alunos.put("Alice", 4);
+        alunos.put("Leonardo", 33);
+        alunos.put("Ladyjane", 27);
+        alunos.put("Eduardo", 25);
+        alunos.put("Lucas", 21);
+
+        Iterator<Map.Entry<String, Integer>> iterator = alunos.entrySet().iterator();
+        while (iterator.hasNext()) {
+            String next = iterator.next().getKey();
+            System.out.println(next);
+        }
+        System.out.println("-------------------------------------------");
+        HashMap<Aluno, Integer> alunoHashMap = new HashMap<>();
+        alunoHashMap.put(new Aluno("Leandro", 34), 1);
+        alunoHashMap.put(new Aluno("Leonardo", 33), 2);
+        alunoHashMap.put(new Aluno("Ladyjane", 27), 3);
+        alunoHashMap.put(new Aluno("Eduardo", 25), 4);
+        alunoHashMap.put(new Aluno("Lucas", 22), 5);
+
+
+        for (Map.Entry<Aluno, Integer> item : alunoHashMap.entrySet()) {
+            System.out.println(item.getKey().nome + " - " + item.getKey().idade);
+        }
+
+    }
+
+    static class Aluno{
+        String nome;
+        Integer idade;
+
+        Aluno(String nome, Integer idade) {
+            this.nome = nome;
+            this.idade = idade;
+        }
+    }
+    
 }
